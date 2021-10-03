ef3dda77f7b1ec8064b1244430721bf92e48dc3b
juls2001 <20184434@student.curtin.edu.au>
2021-10-03 12:15:55 +0800

    removed build files

--- cmake-build-debug/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/julianditthardt/Documents/C++/Mini Projects/LinkedLists/googletest/googletest
-# Build directory: /Users/julianditthardt/Documents/C++/Mini Projects/LinkedLists/cmake-build-debug/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
